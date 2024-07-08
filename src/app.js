d92a79c45799814727a7f3beb6d3a9335d5016fc
Alfredo Cossetin Neto <alfredocossetin@outlook.com>
2024-07-08 19:48:31 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const router = require("./routes");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 app.use(router);
